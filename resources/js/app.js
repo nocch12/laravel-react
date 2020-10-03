29b83f13b638e944ff912851cdcf4f7817773893
nocch12 <k.ohno12.gt@gmail.com>
2020-10-03 10:49:03 +0900

    基本のルーティング作成まで

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Example from './components/Example';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
-if (document.getElementById('app')) {
-    ReactDOM.render(<Example />, document.getElementById('app'));
+const App = () => {
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <Route path="/about" component={About} />
+            <Route exact path="/" component={Home} />
+        </BrowserRouter>
+    );
+};
+
+if (document.getElementById("app")) {
+    ReactDOM.render(<App />, document.getElementById("app"));
 }
